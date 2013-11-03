2ba01e1101c40b8a5cef84022afe4bcbefe5f61b
Bennett Lin <bennettslin@gmail.com>
2013-11-02 22:01:45 -0700

    Initial Commit

---
+++ ManagingViewHierarchyApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ManagingViewHierarchyApp
+//
+//  Created by Bennett Lin on 11/2/13.
+//  Copyright (c) 2013 Bennett Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
